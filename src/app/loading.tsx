6be001d21ae77f9244d6427215bf33d8c864a235
helwyza-f <helwyza@gmail.com>
2025-04-01 15:06:21 +0700

    add loading to server komponen

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+// src/app/profile/loading.tsx
+
+export default function Loading() {
+  return (
+    <div className="flex min-h-screen justify-center items-center">
+      <div className="w-16 h-16 border-4 border-t-transparent border-blue-600 border-solid rounded-full animate-spin"></div>
+    </div>
+  );
+}
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,19 +1,31 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-
 import Link from "next/link";
+import { usePathname } from "next/navigation"; // Import usePathname to detect active path
 
 export default function Navigation() {
+  const pathname = usePathname(); // Get the current path
+
   return (
     <nav className="fixed bottom-0 left-0 w-full flex justify-around bg-black p-4 md:static md:top-0 md:w-full md:flex-row md:justify-center md:gap-10 md:px-20 *:bg-gray-800">
-      <Button asChild variant="ghost" className="md:text-lg">
+      <Button
+        asChild
+        variant="ghost"
+        className={`md:text-lg ${
+          pathname === "/" ? "text-blue-500" : "text-white"
+        }`} // Highlight Home link when active
+      >
         <Link href={"/"}>🏠 Home</Link>
       </Button>
-      {/* <Button asChild variant="ghost" className="md:text-lg">
-        <Link href="/analyze">📷 Analyze</Link>
-      </Button> */}
-      <Button asChild variant="ghost" className="md:text-lg">
+
+      <Button
+        asChild
+        variant="ghost"
+        className={`md:text-lg ${
+          pathname === "/profile" ? "text-blue-500" : "text-white"
+        }`} // Highlight Profile link when active
+      >
         <Link href={"/profile"}>👤 Profile</Link>
       </Button>
     </nav>
